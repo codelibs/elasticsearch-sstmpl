33d3babdd19747ca518fced576beafa5926ec1d7
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2014-10-10 06:34:04 +0900

    add test cases

--- src/test/java/org/codelibs/elasticsearch/sstmpl/ScriptTemplatePluginTest.java
+++ src/test/java/org/codelibs/elasticsearch/sstmpl/ScriptTemplatePluginTest.java
@@ -5,10 +5,15 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+import java.util.Map;
+
 import org.codelibs.elasticsearch.runner.ElasticsearchClusterRunner;
+import org.codelibs.elasticsearch.runner.net.Curl;
+import org.codelibs.elasticsearch.runner.net.CurlResponse;
 import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.client.Client;
 import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.node.Node;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -33,7 +38,6 @@ public class ScriptTemplatePluginTest {
     public void test_search() throws Exception {
 
         assertThat(1, is(runner.getNodeSize()));
-        final Client client = runner.client();
 
         final String index = "sample";
         final String type = "data";
@@ -42,8 +46,54 @@ public class ScriptTemplatePluginTest {
         for (int i = 1; i <= 1000; i++) {
             final IndexResponse indexResponse = runner.insert(index, type,
                     String.valueOf(i), "{\"id\":\"" + i + "\",\"msg\":\"test "
-                            + i + "\",\"counter\":" + i + "}");
+                            + i + "\",\"counter\":" + i + ",\"category\":"
+                            + (i % 100) + "}");
             assertTrue(indexResponse.isCreated());
+        }
+
+        Node node = runner.node();
+
+        String query;
+
+        query = "{\"query\":{\"match_all\":{}}}";
+        try (CurlResponse curlResponse = Curl
+                .post(node, "/" + index + "/" + type + "/_search").body(query)
+                .execute()) {
+            Map<String, Object> contentMap = curlResponse.getContentAsMap();
+            Map<String, Object> hitsMap = (Map<String, Object>) contentMap
+                    .get("hits");
+            assertThat(1000, is(hitsMap.get("total")));
+            assertThat(
+                    10,
+                    is(((List<Map<String, Object>>) hitsMap.get("hits")).size()));
+        }
+
+        query = "{\"template\":{\"query\":{\"match\":{\"{{my_field}}\":\"{{my_value}}\"}},\"size\":\"{{my_size}}\"},"
+                + "\"params\":{\"my_field\":\"msg\",\"my_value\":\"test\",\"my_size\":\"50\"}}";
+        try (CurlResponse curlResponse = Curl
+                .post(node, "/" + index + "/" + type + "/_search/template")
+                .body(query).execute()) {
+            Map<String, Object> contentMap = curlResponse.getContentAsMap();
+            Map<String, Object> hitsMap = (Map<String, Object>) contentMap
+                    .get("hits");
+            assertThat(1000, is(hitsMap.get("total")));
+            assertThat(
+                    50,
+                    is(((List<Map<String, Object>>) hitsMap.get("hits")).size()));
+        }
+
+        query = "{\"lang\":\"mustache\",\"template\":{\"query\":{\"match\":{\"{{my_field}}\":\"{{my_value}}\"}},\"size\":\"{{my_size}}\"},"
+                + "\"params\":{\"my_field\":\"msg\",\"my_value\":\"test\",\"my_size\":\"50\"}}";
+        try (CurlResponse curlResponse = Curl
+                .post(node, "/" + index + "/" + type + "/_search/template")
+                .body(query).execute()) {
+            Map<String, Object> contentMap = curlResponse.getContentAsMap();
+            Map<String, Object> hitsMap = (Map<String, Object>) contentMap
+                    .get("hits");
+            assertThat(1000, is(hitsMap.get("total")));
+            assertThat(
+                    50,
+                    is(((List<Map<String, Object>>) hitsMap.get("hits")).size()));
         }
 
     }
